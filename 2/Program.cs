348794ea29c22237a0b8e854215aa8e88ba299fa
ADM <ADM@A320M-H-CF>
2022-01-29 15:19:24 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:/dota/Dota 2";
+            string[] dirs = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+            for (int i = 0; i < dirs.Length; i++)
+                if (Directory.Exists(dirs[i]))
+                {
+                    Console.WriteLine(dirs[i]);
+                    string[] sfiles = Directory.GetFiles(dirs[i]);
+                    foreach (string f in sfiles)
+                    {
+                        Console.WriteLine(f);
+                    }
+                    string[] subdirs = Directory.GetDirectories(dirs[i]);
+                    foreach (string s in subdirs)
+                    {
+                        Console.WriteLine(s);
+                        string[] stfiles = Directory.GetFiles(s);
+                        foreach (string f in stfiles)
+                        {
+                            Console.WriteLine(f);
+                        }
+                    }
+                }
+ 
+            
+        }
+
+    }
+}
---
+++ 8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:/2018/log.txt";
+            if (!File.Exists(path))
+                File.Create(path);
+            StreamWriter sw = new StreamWriter(path);
+            
+                int[] array = new int[10];
+                Random random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = random.Next(0, 10);
+                    sw.WriteLine(" {0} ", array[i]);
+                }
+               sw.Close();
+            int S = 0;
+            using StreamReader sr = new StreamReader(path);
+            {
+                int[] array2 = new int[10];
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    array2[i] = Convert.ToInt32(sr.ReadLine());
+                    S += array[i];
+                }
+            }
+            Console.WriteLine(S);
+        }
+    }
+}
+
